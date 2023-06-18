c37bbda99e1698347964a90d11854669ec732f53
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-18 22:24:32 +0500

    Move To Pages Directory

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -48,7 +48,7 @@ export default NextAuth({
         })
     ],
     pages: {
-        signIn: "/"
+        signIn: "/auth"
     },
     debug: process.env.NODE_ENV === "development",
     session: { strategy: "jwt" },
--- app/api/register.ts
+++ app/api/register.ts
@@ -1,11 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import prismadb from "@/lib/prismadb";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/lib/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
     try {
-
         if (req.method !== 'POST') {
             return res.status(405).end();
         }
@@ -16,10 +14,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             where: {
                 email
             }
-        });
+        })
 
         if (existingUser) {
-            return res.status(422).json({ error: "Email Already Exists" });
+            return res.status(422).json({ error: 'Email taken' });
         }
 
         const hashedPassword = await bcrypt.hash(password, 12);
@@ -29,16 +27,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 email,
                 name,
                 hashedPassword,
-                image: "",
+                image: '',
                 emailVerified: new Date(),
             }
-        });
+        })
 
         return res.status(200).json(user);
-
     } catch (error) {
-
-        return res.status(400).json({ error: "Somethin Went Worng!" });
-
+        return res.status(400).json({ error: `Something went wrong: ${error}` });
     }
-};
+}
