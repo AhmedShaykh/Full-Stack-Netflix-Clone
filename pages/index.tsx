9314e1d7515f02282eb74ba816440331c187f7ab
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-21 14:35:15 +0500

    Add Logout Functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
+import React from "react";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+
+    const session = await getSession(context);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/auth",
+                permanent: false
+            }
+        }
+    };
+
+    return {
+        props: {}
+    };
+};
+
 const Home = () => {
     return (
         <>
-        <h1>
-            Hello
-        </h1>
+            <h1 className="text-white">
+                Hello
+            </h1>
+
+            <button
+                className="h-10 w-20 bg-white"
+                onClick={() => signOut()}
+            >
+                Sign Out
+            </button>
         </>
     )
 };
